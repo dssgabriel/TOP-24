1970fea2febc5bf41abccc433b9a3e60f96aa30e
Gabriel Dos Santos <dss.gabriel@protonmail.com>
2024-04-09 01:07:01 +0200

    lab02: fix using `mdspan_2d` shorthand

--- LAB_02_03-SHARED_MEMORY/exercises/cross-stencil/include/check_result.hpp
+++ LAB_02_03-SHARED_MEMORY/exercises/cross-stencil/include/check_result.hpp
@@ -1,14 +1,10 @@
 #pragma once
 
-#include "../ext/mdspan/include/mdspan/mdspan.hpp"
-
-using namespace Kokkos;
-using dextents_2d = extents<size_t, dynamic_extent, dynamic_extent>;
+#include "mdspan_type_util.hpp"
 
 namespace cross_stencil {
 /// Check if the results between the non-blocked and cache-blocked implementations match.
 auto check_result(
-    mdspan<double, dextents_2d> const& result_non_blocked,
-    mdspan<double, dextents_2d> const& result_blocked
+    mdspan_2d<double> const& result_non_blocked, mdspan_2d<double> const& result_blocked
 ) -> void;
 } // namespace cross_stencil
